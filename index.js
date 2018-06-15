3b1b2949286b5bd92049a5059763d1ac7040d1ac
Kohach <alexis.montagne@hotmail.fr>
2018-06-15 16:36:32 +0200

    all database table with associations

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 const express = require('express');
 const app = express();
 const router = require('./src/router');
+require('dotenv').config();
 
-app.use('/', router);
-app.listen('3000');
+const db = require('./src/models/index');
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log("Success");
+  app.use('/', router);
+  app.listen('3000');
+}).catch(err => {
+  console.log(err);
+})
+

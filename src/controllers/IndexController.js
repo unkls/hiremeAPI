12f5c034204d50dbcd86001d1876de0f9677bd53
Kohach <alexis.montagne@hotmail.fr>
2018-07-10 16:47:57 +0200

    Base structure with sequelize

--- src/controllers/IndexController.js
+++ src/controllers/IndexController.js
@@ -1,8 +1,12 @@
 const Controller = require('../core/Controller');
+const db = require('../models');
 
 class IndexController extends Controller{
     index(req, res, next){
-        res.send("Hello index");
+        //res.send("Hello index");
+        db.User.findAll().then((users) => {
+            res.send(users);
+        });
     }
 }
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
-const indexRoute = require ('./routes/indexRoute.js');
+const indexRoute = require ('./routes/indexRoute');
+const candidateRoute = require ('./routes/candidateRoute');
+const userRoute = require('./routes/userRoute');
 
 module.exports = (req, res, next) => {
     req.app.use('/', indexRoute);
+    req.app.use('/candidates', candidateRoute);
+    req.app.use('/users', userRoute);
     next();
 };

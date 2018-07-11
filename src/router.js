3f1967c693128c9437a829f6ed099f9ca90e18e6
Kohach <alexis.montagne@hotmail.fr>
2018-07-11 09:25:26 +0200

    api version

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 const indexRoute = require ('./routes/indexRoute');
 const candidateRoute = require ('./routes/candidateRoute');
 const userRoute = require('./routes/userRoute');
+const apiVersion = '/v1'
 
 module.exports = (req, res, next) => {
-    req.app.use('/', indexRoute);
-    req.app.use('/candidates', candidateRoute);
-    req.app.use('/users', userRoute);
+    req.app.use(apiVersion + '/', indexRoute);
+    req.app.use(apiVersion + '/candidates', candidateRoute);
+    req.app.use(apiVersion + '/users', userRoute);
     next();
 };
